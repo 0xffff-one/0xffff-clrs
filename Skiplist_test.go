e4b55ae1296a05811507f8dbe2794cd46d693b3d
Cybsac <30456774+Machinesaac@users.noreply.github.com>
2020-02-26 10:39:27 +0800

    Add files via upload

---
+++ Skiplist_test.go
@@ -0,0 +1,15 @@
+package SkipList
+
+
+import (
+	"testing"
+)
+func TestInsertion(t *testing.T){
+	head := SkipListNode{}
+	list := SkipList{head:&head, tail:nil} 
+	for i:=0;i<=100;i++{
+		if (*SkipList).insert(&list,i,i){
+			t.Error(`Insertion failed`)
+		}
+	}
+}
